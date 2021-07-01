ef25ee8fe7f5cb1508d36b376d7c1bade450d70d
TolmaSh <m0v4anua@gmail.com>
2021-07-01 22:22:37 +0300

    Structure : add about and revies section with styles and add swiper library

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,15 @@ function test(a, b) {
     return a + b
 }
 
-console.log(test(5, 2));
-console.log(test(2, 4));
-console.log(test(9, 1));
-
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    loop: true,
+  
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
 //# sourceMappingURL=script.js.map
